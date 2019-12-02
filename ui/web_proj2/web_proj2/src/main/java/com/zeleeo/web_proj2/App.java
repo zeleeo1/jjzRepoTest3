96387314166d4480aad8618b67017567a2fc3aec
jxz008 <jjz62@yahoo.com>
2019-12-01 20:06:40 -0500

    init

---
+++ ui/web_proj2/web_proj2/src/main/java/com/zeleeo/web_proj2/App.java
@@ -0,0 +1,13 @@
+package com.zeleeo.web_proj2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
